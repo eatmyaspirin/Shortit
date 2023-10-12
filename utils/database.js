79ec3af78073fb06f97b2d10019b0677c1a2e8f0
Ea <ritvikashesh2000@gmail.com>
2023-10-12 19:57:54 +0530

    refactor db calls

--- utils/database.js
+++ utils/database.js
@@ -1,48 +1,45 @@
 const sqlite = require('sqlite3');
 
-const db = new sqlite.Database('./utils/db', (err) => {
+const db = new sqlite.Database('./db/db', (err) => {
     if (err) {
         console.error(err.message)
         throw err
-      }else{
-          console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE user (
+    } else {
+        console.log('Connected to the SQLite database, configuring tables')
+        db.exec(
+            `CREATE TABLE user (
             userId text PRIMARY KEY,
             username text UNIQUE, 
             password text, 
             CONSTRAINT username_unique UNIQUE (username)
-            )`,
-        (err) => {
-            if (err) {
-                console.error('1st', err);
-            } 
-            db.run(`CREATE TABLE url (
+            );
+            CREATE TABLE url (
                 urlId text PRIMARY KEY,
                 url text, 
                 shortUrl text UNIQUE,
                 userId text,
                 CONSTRAINT shorturl_unique UNIQUE (shorturl),
                 FOREIGN KEY (userId) REFERENCES user(userId)
-                )`,
+            );
+            CREATE TABLE stats (
+                urlId text,
+                userId text, 
+                visitors text,
+                FOREIGN KEY (urlId) REFERENCES url(urlId),
+                FOREIGN KEY (userId) REFERENCES user(userId)
+            );            
+            `,
             (err) => {
                 if (err) {
-                    console.error('2nd', err);
-                } 
-                db.run(`CREATE TABLE stats (
-                    urlId text,
-                    userId text, 
-                    visitors text,
-                    FOREIGN KEY (urlId) REFERENCES url(urlId),
-                    FOREIGN KEY (userId) REFERENCES user(userId)
-                    )`,
-                (err) => {
-                    if (err) {
-                        console.error('3rd', err);
+                    const { errno } = err;
+                    if (errno === 1) {
+                        console.error(err.message, 'configuration not needed, skipping');
+                    } else {
+                        console.log(err);
                     }
-                }); 
+                }
             });
-        });    
-      }
+    }
 })
 
 module.exports = db;

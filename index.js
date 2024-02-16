f4147dc5aa7f4629f7e6d94a427f7590d6881752
Ea <eatmyaspirin@gmail.com>
2024-02-16 12:54:24 +0530

    minor fixes

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const { isLoggedIn, isAdmin } = require("./middleware/auth");
 
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ["http://localhost:5173", "https://shortit-front.vercel.app/"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://shortit-front.vercel.app/",
+];
 
 const unSecure = ["/create"];
-console.log(process.env.JWT_SECRET, `${process.env.JWT_SECRET}`)
 app.use(cookieParser());
 app.use((req, res, next) => {
   const origin = req.headers.origin;
+  console.log(req.url, 'first');
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
@@ -20,18 +23,21 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-  next();
-});
+    );
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+    next();
+  });
+  
+  
+app.use("/test", (req, res)=>res.send("test"));
+app.use("/api", require("./routes/url/getShortURL"));
 
-app.use("/", require("./routes/url/getShortURL"));
-app.use("/login", require("./routes/loginUser"));
-app.use("/register", require("./routes/registerUser"));
-app.use("/create", isLoggedIn, require("./routes/url/createShortURL"));
-app.use("/user", isLoggedIn, require("./routes/user.js"));
-app.use("/admin", [isLoggedIn, isAdmin], require("./routes/admin.js"));
-app.use("/logout", (req, res) => {
+app.use("/api/login", require("./routes/loginUser"));
+app.use("/api/register", require("./routes/registerUser"));
+app.use("/api/create", isLoggedIn, require("./routes/url/createShortURL"));
+app.use("/api/user", isLoggedIn, require("./routes/user.js"));
+app.use("/api/admin", [isLoggedIn, isAdmin], require("./routes/admin.js"));
+app.use("/api/logout", (req, res) => {
   res.clearCookie("jwt");
   res.status(200).json({
     message: "Logged out successfully",
